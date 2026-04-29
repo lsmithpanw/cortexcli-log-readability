2f242ae47892e2473789a90e6c17879e16adcb4a
Lindsey Smith <lindsey.smith@gmail.com>
2026-04-29 11:46:13 -0700

    Support 0.31.0 log file location change

--- ccl.sh
+++ ccl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-last_log_file=~/.cortexcli/logs/$(ls -t ~/.cortexcli/logs/ | head -n1)
+last_log_file=~/.cortexcli/cortexcli-log/$(ls -t ~/.cortexcli/cortexcli-log/ | head -n1)
 echo ${last_log_file}:
 cat $last_log_file | jq -r '[(.severity, .message)] | @csv'
